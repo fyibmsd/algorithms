8b387ae1948a4943f5f107fc8cdd048fa2869575
fyibmsd <fyibmsd@gmail.com>
2019-04-07 09:21:38 +0800

    fix

--- src/algorithms/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export { lcs } from './string/Lcs';
-
-export { createMultiDim } from './Array';
-
-export { listToString, reverseRecursion, reverseNonRecursion } from './ListReverse';
