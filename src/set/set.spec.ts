283b1915d41a4dec9d4a1f09736907d19c856cda
fyibmsd <fyibmsd@gmail.com>
2019-04-08 18:20:13 +0800

    sets union, intersection and difference

--- src/set/set.spec.ts
+++ src/set/set.spec.ts
@@ -37,4 +37,42 @@ describe('test set', () => {
         expect(set.add(5)).be.false;
     });
 
+    it('should return two sets union', () => {
+        const firstSet = new Set<number>();
+        const secondSet = new Set<number>();
+
+        range(1, 3).map(v => firstSet.add(v));
+        range(4, 6).map(v => secondSet.add(v));
+
+        const unionSet = firstSet.union(secondSet);
+
+        expect(unionSet.size()).equal(6);
+        range(1, 6).map(v => expect(unionSet.has(v)).be.true);
+    });
+
+    it('should return two sets intersection', () => {
+        const firstSet = new Set<number>();
+        const secondSet = new Set<number>();
+
+        range(1, 5).map(v => firstSet.add(v));
+        range(3, 7).map(v => secondSet.add(v));
+
+        const intersectionSet = firstSet.intersection(secondSet);
+
+        expect(intersectionSet.size()).equal(3);
+        range(3, 5).map(v => expect(intersectionSet.has(v)).be.true);
+    });
+
+    it('should return two sets difference', () => {
+        const firstSet = new Set<number>();
+        const secondSet = new Set<number>();
+
+        range(1, 5).map(v => firstSet.add(v));
+        range(3, 7).map(v => secondSet.add(v));
+
+
+        expect(firstSet.difference(secondSet).values()).deep.equals(['1', '2']);
+        expect(secondSet.difference(firstSet).values()).deep.equals(['6', '7']);
+    });
+
 });
--- src/set/set.ts
+++ src/set/set.ts
@@ -34,4 +34,34 @@ export default class Set<T> {
     isEmpty() {
         return this.size() === 0;
     }
+
+    union(otherSet: Set<T>): Set<T> {
+        const unionSet = new Set<T>();
+
+        this.values().map(key => unionSet.add(this.items[key]));
+
+        otherSet.values().map(key => unionSet.add(otherSet.items[key]));
+
+        return unionSet;
+    }
+
+    intersection(otherSet: Set<T>): Set<T> {
+        const intersectionSet = new Set<T>();
+
+        this.values().map(key =>
+            otherSet.has(this.items[key]) && intersectionSet.add(this.items[key])
+        );
+
+        return intersectionSet;
+    }
+
+    difference(otherSet: Set<T>): Set<T> {
+        const differenceSet = new Set<T>();
+
+        this.values().map(key =>
+            otherSet.has(this.items[key]) === false && differenceSet.add(this.items[key])
+        );
+
+        return differenceSet;
+    }
 }
